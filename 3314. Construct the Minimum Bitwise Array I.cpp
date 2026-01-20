10008e1ae712e9037f790d5b9a36cc1dbc1ebcdf
Lal Singh Jhala <lalsinghjhala1999@gmail.com>
2026-01-20 23:55:59 +0530

    3314. Construct the Minimum Bitwise Array I

---
+++ 3314. Construct the Minimum Bitwise Array I.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i++) {
+            bool found = false;
+            for(int x = 0; x < nums[i]; x++) {
+                if((x | (x+1)) == nums[i]) {
+                    ans.push_back(x);
+                    found = true;
+                    break;
+                }
+            }
+            if(found == false) {
+                ans.push_back(-1);
+            }
+         }
+            return ans;
+    }
+};
