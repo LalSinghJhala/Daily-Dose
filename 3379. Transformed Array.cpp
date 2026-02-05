c495a00593fe8a597a105f78273fe3b5095cb897
Lal Singh Jhala <lalsinghjhala1999@gmail.com>
2026-02-05 16:18:58 +0530

    3379. Transformed Array

---
+++ 3379. Transformed Array.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n);
+
+        for(int i = 0; i < n; i++) {
+            int idx = (i + nums[i]) % n;
+            if(idx < 0) idx += n;   // normalize
+            res[i] = nums[idx];
+        }
+        return res;
+    }
+};
