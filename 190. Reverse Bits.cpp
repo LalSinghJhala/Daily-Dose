5f0abfa60b623da0b054533cbdab38bd4d7b6b05
Lal Singh Jhala <lalsinghjhala1999@gmail.com>
2026-02-16 06:36:31 +0530

    190. Reverse Bits

---
+++ 190. Reverse Bits.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int reverseBits(uint32_t n) {
+        int ans=0;
+        for(; n; n&=(n-1)){
+            ans+=1<<(31-countr_zero(n));
+        }
+        return ans;
+    }
+};
