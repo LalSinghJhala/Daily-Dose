99ee922e8e5eed8022ed6702941c01777214af19
Lal Singh Jhala <lalsinghjhala1999@gmail.com>
2026-03-06 23:57:58 +0530

    1784. Check if Binary String Has at Most One Segment of Ones

---
+++ 1784. Check if Binary String Has at Most One Segment of Ones.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool checkOnesSegment(string s) {
+        int n = s.length();
+        int count = 0;
+        int i = 0;
+
+        while(i < n) {
+            if(s[i] == '1') {
+                count++;
+                while(i < n && s[i] == '1') {
+                    i++;
+                }
+            }
+            else {
+                i++;
+            }
+
+            if(count > 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
