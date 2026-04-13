4b20c9bd5bfb0f41e3f212498ed692f42a30506c
Lal Singh Jhala <lalsinghjhala1999@gmail.com>
2026-04-13 09:27:28 +0530

    1848. Minimum Distance to the Target Element

---
+++ 1848. Minimum Distance to the Target Element.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int ans = INT_MAX;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == target) {
+                ans = min(ans, abs(i-start));
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+int minDist = INT_MAX;
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i] == target) {
+                minDist = min(minDist,abs(i-start));
+                
+            }
+
+        }
+        return minDist;
+
+*/
